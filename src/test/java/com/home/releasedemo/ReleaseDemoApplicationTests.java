af2bae7f50413328e5ff073bc0a36b8c3b35b0e4
saliaksandr <saliaksandr@vmware.com>
2023-07-02 20:50:18 +0400

    init

---
+++ src/test/java/com/home/releasedemo/ReleaseDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.releasedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReleaseDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
